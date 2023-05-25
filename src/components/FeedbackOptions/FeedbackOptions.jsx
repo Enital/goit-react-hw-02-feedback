e825258f1ed3d5515eed990957007eb736fd74de
Enital <117684114+Enital@users.noreply.github.com>
2023-05-25 17:10:48 +0300

    8

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import css from './feedbackOptions.css';
+// import css from './feedbackOptions.css';
 
 function FeedbackOptions ({ onLeaveFeedback }) {
     return (
-    <ul className={css.buttons}>
-        <button className={css.button} onClick={onLeaveFeedback}>Good</button>
-        <button className={css.button} onClick={onLeaveFeedback}>Neutral</button>
-        <button className={css.button} onClick={onLeaveFeedback}>Bad</button>
+    <ul>
+        <button onClick={onLeaveFeedback}>Good</button>
+        <button onClick={onLeaveFeedback}>Neutral</button>
+        <button onClick={onLeaveFeedback}>Bad</button>
     </ul>
     )
 };
